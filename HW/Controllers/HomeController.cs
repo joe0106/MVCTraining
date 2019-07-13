2107490614d12045a7a6e67e36b1c60f8dd39fdb
yi-cheng wang <beebub0106@gmail.com>
2019-07-13 18:23:38 +0800

    0706作業

--- HW/Controllers/HomeController.cs
+++ HW/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HW.Models.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,7 +11,24 @@ namespace HW.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            List<RecordsViewModel> recordList = new List<RecordsViewModel>();
+            #region 亂數產生100筆假資料    
+            Random random = new Random();
+
+            for (int i = 0; i < 100; i++)
+            {
+                RecordsViewModel record = new RecordsViewModel();
+                record.Category = random.Next(0, 2) == 1 ? "收入" : "支出";
+                record.Price = random.Next(1, 100) * 10;
+
+                DateTime start = new DateTime(2018, 1, 1);
+                int range = (DateTime.Today - start).Days;
+                record.BuyDate = start.AddDays(random.Next(range));
+
+                recordList.Add(record);
+            }
+            #endregion
+            return View(recordList);
         }
 
         public ActionResult About()
---
+++ HW/Models/ViewModels/RecordsViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace HW.Models.ViewModels
+{
+    public class RecordsViewModel
+    {
+        [Display(Name = "類別")]
+        public string Category { get; set; }
+
+        [Display(Name = "價格")]
+        public int Price { get; set; }
+
+        [Display(Name = "日期")]
+        public DateTime BuyDate { get; set; }
+
+        [Display(Name = "備註")]
+        public string Remarks { get; set; }
+    }
+}
